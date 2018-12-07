6a899da47f63da24e1eb28c590026ecaa064e035
rsbuca <rsb.git@coder.hr>
2018-12-07 03:31:44 +0100

    Fixes /statuses controller to make more sense.

--- rest-api/src/api/controllers/statuses-controller.js
+++ rest-api/src/api/controllers/statuses-controller.js
@@ -30,9 +30,11 @@ router.post(mapping + '/rnd', (req, res) => {
     status.save((err) => {
         if (err) {
             res.status(500).send(err);
+            return;
         }
 
         res.json({ message: 'Random status generated successfully.', status: status });
+        return;
     });
 });
 
@@ -40,117 +42,23 @@ router.get(mapping, (req, res) => {
     Status.find((err, statuses) => {
         if (err) {
             res.status(500).send('Unexpected error occured while fetching statuses.');
+            return;
         }
 
-        res.json({ statuses: statuses });
+        res.json({ message: 'FOR TESTING PURPOSES ONLY', statuses: statuses });
+        return;
     });
 });
 
-router.post(mapping,
-    security.authenticationMiddleware,
-    security.accessFrequencyLimiterMiddlewareByToken,
-    (req, res) => {
-    /**
-     * Status
-     * statusId -> generated automatically
-     * text
-     * userId
-     */
-
-    if (!req.body.userId) {
-        req.status(400).send('Malformed request. Please provide userId this status should be added to.');
-    }
-    if (!req.body.text) {
-        req.status(400).send('Malformed request. Please provide text for this status.');
-    }
-
-    const statusId = utils.randomId();
-    const userId = String(req.body.userId);
-    const text = String(req.body.text);
-
-    // first find if user with given userId exists
-    User.find((err, users) => {
+router.delete(mapping, (req, res) => {
+    User.remove({}, (err, statuses) => {
         if (err) {
-            res.status(500).send('Unexpected error while checking if the userId given is valid.');
+            res.status(500).send('Unexpected error while deleting all statuses from all users.');
+            return;
         }
 
-        let userFound = false;
-        let ui = -1;
-        for (let i in users) {
-            if (users[i].userId === userId) {
-                userFound = true;
-                ui = i;
-            }
-        }
-
-        if (!userFound) {
-            res.status(400).send('Please provide existing userId.');
-        }
-
-        const status = new Status();
-        status.statusId = statusId;
-        status.text = text;
-        status.userId = userId;
-
-        status.save((err) => {
-            if (err) {
-                res.status(500).send('Unexpected error while saving new status.');
-            }
-
-            res.json({ message: 'Status saved successfully.', status: status });
-        });
-    });
-});
-
-router.put(mapping,
-    security.authenticationMiddleware,
-    security.accessFrequencyLimiterMiddlewareByToken,
-    (req, res) => {
-    if (!req.body.statusId) {
-        req.status(400).send('Malformed request. Please provide statusId');
-    }
-    if (!req.body.userId) {
-        req.status(400).send('Malformed request. Please provide userId this status should be added to.');
-    }
-    if (!req.body.text) {
-        req.status(400).send('Malformed request. Please provide text for this status.');
-    }
-
-    const statusId = String(req.body.statusId);
-    const userId = String(req.body.userId);
-    const text = String(req.body.text);
-
-    Status.update({
-        statusId: statusId
-        }, {
-            text: text,
-        }, (err, user) => {
-            if (err) {
-                res.status(500).send('Unexpected server error while updating status text.');
-            }
-
-            res.json({ message: 'Status successfully updated.', user: user });
-    });
-});
-
-router.delete(mapping,
-    security.authenticationMiddleware,
-    security.accessFrequencyLimiterMiddlewareByToken,
-    (req, res) => {
-    if (!req.body.statusId) {
-        req.status(400).send('Malformed request. Please provide statusId');
-    }
-
-    const statusId = String(req.body.statusId);
-
-    Status.remove({
-        statusId: statusId
-    }, function(err, status) {
-        if (err) {
-            res.status(500).send(err);
-        }
-
-        res.json({ message: 'Successfully deleted', status: status });
+        res.json({ message: 'deleted all statuses from all users', statuses: statuses });
+        return;
     });
 });
 
